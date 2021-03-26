585da42641ec12d79254d404df0cc19aaade39ff
datsfilipe <datsfilipe@gmail.com>
2021-03-26 17:30:17 -0300

    update error solved

--- app/Http/Controllers/ControllerRegister.php
+++ app/Http/Controllers/ControllerRegister.php
@@ -6,12 +6,8 @@ use Illuminate\Http\Request;
 
 class ControllerRegister extends Controller
 {
-    public function saveRegister(Request $request)
+    public function saveRegister()
     {
-        $name = $request -> name;
-        $date = $request -> date;
-        $password = $request -> password;
-        $id = $request -> id;
-        return view('finish-register', compact('name', 'date', 'password', 'id'));
+        return view('finish-register');
     }
 }
